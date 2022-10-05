6255cc129a589bf88c74e973bb77c6da075db760
Rinchen <rinchentb>
2022-10-05 11:46:11 +0530

    commit

---
+++ 8. Prime number/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int isPrime = 1;
+    for (int i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
+            isPrime = 0;
+            break;
+        }
+    }
+    isPrime == 1 ? cout << "Prime" : cout << "Non Prime";
+    return 0;
+}
---
+++ 9. Prime number within a given range/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+    int isPrime;
+    for (int i = m; i <= n; i++)
+    {
+        isPrime = 1;
+        if (i <= 1)
+            isPrime = 0;
+        for (int j = 2; j <= i / 2; j++)
+        {
+            if (i % j == 0)
+            {
+                isPrime = 0;
+                break;
+            }
+        }
+        if (isPrime == 1)
+            cout << i << "\t";
+    }
+    return 0;
+}
